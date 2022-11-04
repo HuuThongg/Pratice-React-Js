66fbf9cc5c0d9cd5966ca9cc87f8f339c8a2c6d4
HuuThongg <lehuuthong0506@gmail.com>
2022-11-03 21:21:06 -0700

    first commit expense-app

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -7,13 +7,13 @@ const ExpensesList = (props)=>{
    return <ul className="expenses-list">
       {
          props.items.map((expense) => (
-         <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-         />
-      ))
+            <ExpenseItem
+               key={expense.id}
+               title={expense.title}
+               amount={expense.amount}
+               date={expense.date}
+            />
+         ))
       }
    </ul>
 }
