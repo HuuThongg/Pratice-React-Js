25b34f55324b8f5212bd041f6467c17d0fdd1ac0
HuuThongg <lehuuthong0506@gmail.com>
2022-11-06 20:21:29 -0800

    first  commit

---
+++ build/precache-manifest.fe75237ee21ff904d2ed3e616769ce01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8968373fdd53f4535ed",
+    "url": "/static/css/main.21f20b04.chunk.css"
+  },
+  {
+    "revision": "c8968373fdd53f4535ed",
+    "url": "/static/js/main.46af523d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bdc7826b8538f1021231",
+    "url": "/static/js/2.971d4b59.chunk.js"
+  },
+  {
+    "revision": "65e72ae968b48588ac1d14e7b0dace3d",
+    "url": "/index.html"
+  }
+];
